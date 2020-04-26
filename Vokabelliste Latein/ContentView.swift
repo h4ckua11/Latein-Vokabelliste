f27b941a1ffaf8a5c9dea76023866f7b2c017e3b
h4ckua11 <h4ckau11@gmx.net>
2020-04-26 14:22:53 +0200

    Initial Commit

---
+++ Vokabelliste Latein/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Vokabelliste Latein
+//
+//  Created by h4ckua11 on 26.04.20.
+//  Copyright © 2020 h4ckua11. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
